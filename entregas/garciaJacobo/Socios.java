808d8066ccf26acb0836864e8f513bc6495585d3
jacobogarcia-04 <jacobo.garcia@alumnos.uneatlantico.es>
2026-03-28 19:48:15 +0100

    Se implementa la vista privada de la clase para su correcto funcionamiento. Se añade lógica para hacer 'vivir' al método preguntarEstadoInscripcion mediante un método privado que utiliza una variable booleana con equals para validar si la inscripción está activa.

--- entregas/garciaJacobo/Socios.java
+++ entregas/garciaJacobo/Socios.java
@@ -1,8 +1,42 @@
 package entregas.garciaJacobo;
 
 public class Socios {
+
     private String nombreSocios;
     private String dni;
     private String fecha;
     private String estadoInscripcion;
+
+    public Socios(String nombreSocios, String dni, String fecha, String estadoInscripcion) {
+        this.nombreSocios = nombreSocios;
+        this.dni = dni;
+        this.fecha = fecha;
+        this.estadoInscripcion = estadoInscripcion;
+    }
+
+    private boolean estadoActivo() {
+
+        boolean alta = this.estadoInscripcion.equals("Alta");
+        return alta;
+    }
+
+    public String preguntarNombreSocios() {
+        return this.nombreSocios;
+    }
+
+    public String preguntarDNI() {
+        return this.dni;
+    }
+
+    public String preguntarFechaInscripcion() {
+        return this.fecha;
+    }
+
+    public String preguntarEstadoInscripcion() {
+        if (this.estadoActivo()) {
+            return "Estado: El socio " + this.nombreSocios + " está de ALTA.";
+        } else {
+            return "Estado: El socio " + this.nombreSocios + " está de BAJA.";
+        }
+    }
 }
