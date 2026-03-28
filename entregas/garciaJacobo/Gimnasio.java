04a028eb4750db6640a758649b5816310f96b8ec
jacobogarcia-04 <jacobo.garcia@alumnos.uneatlantico.es>
2026-03-28 12:34:03 +0100

    Se implementa la vista publica de la clase Gimnasio para su correcto funcionamiento. Asi mismo como la creacion del objeto console para utilizar la clase Consola en vez de usar el System.out.println

--- entregas/garciaJacobo/Gimnasio.java
+++ entregas/garciaJacobo/Gimnasio.java
@@ -2,4 +2,17 @@ package entregas.garciaJacobo;
 
 public class Gimnasio {
     private String nombreGimnasio;
+
+    public Gimnasio(String nombreGimnasio) {
+        this.nombreGimnasio = nombreGimnasio;
+    }
+
+    public String nombrarGimnasio() {
+        return this.nombreGimnasio;
+    }
+
+    public void mostrarNombreGimnasio() {
+        Console console = new Console();
+        console.writeln("Gimnasio: " + this.nombreGimnasio);
+    }
 }
