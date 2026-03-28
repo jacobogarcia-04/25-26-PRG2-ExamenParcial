5d166d231d023187ce0dfde69eace12f3cff8962
jacobogarcia-04 <jacobo.garcia@alumnos.uneatlantico.es>
2026-03-28 13:54:59 +0100

    Se agrega el Constructor asi como la la vista publica de la clase Actividades.

--- entregas/garciaJacobo/Actividades.java
+++ entregas/garciaJacobo/Actividades.java
@@ -5,4 +5,33 @@ public class Actividades {
     private String monitor;
     private int aforo;
 
+    public Actividades(String actividad, String monitor, int aforo) {
+        this.actividad = actividad;
+        this.monitor = monitor;
+        this.aforo = aforo;
+    }
+
+    public String nombrarActividad() {
+        return this.actividad;
+    }
+
+    public String nombrarMonitor() {
+        return this.monitor;
+    }
+
+    public void mostrarAforo() {
+        Console console = new Console();
+        console.writeln("Aforo de " + this.actividad + ": " + this.aforo);
+    }
+
+    public void mostrar() {
+        Console console = new Console();
+        console.writeln(this.actividad + " imparte " + this.monitor);
+    }
+
+    public void agregarActividad() {
+        Console console = new Console();
+        console.writeln("Procesando inscripción en " + this.actividad);
+        console.writeln("Resultado: El socio ha sido vinculado a " + this.actividad + " (Aforo: " + this.aforo + ")");
+    }
 }
