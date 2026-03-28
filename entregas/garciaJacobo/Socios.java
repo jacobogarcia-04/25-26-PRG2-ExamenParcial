6451200be765ed433d0a58674f48471292dfd816
jacobogarcia-04 <jacobo.garcia@alumnos.uneatlantico.es>
2026-03-28 19:37:05 +0100

    Se añaden los atributos privados a la clase Socios

--- entregas/garciaJacobo/Socios.java
+++ entregas/garciaJacobo/Socios.java
@@ -1,5 +1,8 @@
 package entregas.garciaJacobo;
 
 public class Socios {
-    
+    private String nombreSocios;
+    private String dni;
+    private String fecha;
+    private String estadoInscripcion;
 }
