2fe13c0bfc261254d12e7a6312546fa3411a6abe
jacobogarcia-04 <jacobo.garcia@alumnos.uneatlantico.es>
2026-03-30 11:57:40 +0200

    Se añade el contructor Reservas a la clase

--- entregas/garciaJacobo/Reservas.java
+++ entregas/garciaJacobo/Reservas.java
@@ -4,4 +4,10 @@ public class Reservas {
     private String nombre;
     private String actividad;
     private String fecha;
+
+    public Reservas(String nombre, String actividad, String fecha) {
+        this.nombre = nombre;
+        this.actividad = actividad;
+        this.fecha = fecha;
+    }
 }
