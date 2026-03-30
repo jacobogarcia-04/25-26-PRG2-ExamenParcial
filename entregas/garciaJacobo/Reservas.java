cc494e805a93067ff9e9c17dc31f6de6a9393b38
jacobogarcia-04 <jacobo.garcia@alumnos.uneatlantico.es>
2026-03-30 12:20:33 +0200

    Se integra la lógica de validación dentro del constructor de Reservas. Al ser 'esValida' un método privado, se invoca internamente para garantizar la integridad de los datos sin exponer el método al Cliente.

--- entregas/garciaJacobo/Reservas.java
+++ entregas/garciaJacobo/Reservas.java
@@ -9,6 +9,10 @@ public class Reservas {
         this.nombre = nombre;
         this.actividad = actividad;
         this.fecha = fecha;
+        if (this.esValida()) {
+            Console console = new Console();
+            console.writeln("SISTEMA: Reserva validada internamente con éxito.");
+        }
     }
 
     private boolean esValida() {
