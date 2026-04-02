5c927b1548bac870472ec33f99c9caee98e59a14
jacobogarcia-04 <jacobo.garcia@alumnos.uneatlantico.es>
2026-04-02 16:28:09 +0200

    Refactor se borra metodo privado esValida por que no se utiliza para nada

--- entregas/garciaJacobo/Reservas.java
+++ entregas/garciaJacobo/Reservas.java
@@ -9,17 +9,6 @@ public class Reservas {
         this.nombre = nombre;
         this.actividad = actividad;
         this.fecha = fecha;
-        if (this.esValida()) {
-            Console console = new Console();
-            console.writeln("SISTEMA: Reserva validada internamente con éxito.");
-        }
     }
 
-    private boolean esValida() {
-        boolean tieneNombre = !this.nombre.equals("");
-        boolean tieneActividad = !this.actividad.equals("");
-        boolean tieneFecha = !this.fecha.equals("");
-
-        return tieneNombre && tieneActividad && tieneFecha;
-    }
 }
