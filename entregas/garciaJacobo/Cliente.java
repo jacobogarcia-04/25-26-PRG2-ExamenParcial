d4af22774bd8c1e78b1009c1251ab49853286e0b
jacobogarcia-04 <jacobo.garcia@alumnos.uneatlantico.es>
2026-04-02 17:39:01 +0200

    feat: añadir segundo socio y comprobar bloqueo de reserva por aforo completo.

--- entregas/garciaJacobo/Cliente.java
+++ entregas/garciaJacobo/Cliente.java
@@ -8,9 +8,10 @@ public class Cliente {
         Console console = new Console();
 
         Gimnasio gimnasio = new Gimnasio("FitLife Center");
-        Actividades yoga = new Actividades("Yoga", "Elena Ruiz", 20);
+        Actividades yoga = new Actividades("Yoga", "Elena Ruiz", 1);
         Horario horYoga = new Horario("Lun/Vie", 10.0, 12.0);
         Socios socio1 = new Socios("Juan García", "12345678A", "01/04/2025", "ALTA");
+        Socios socio2 = new Socios("Pablo Martinez ", "12345678A", "05/04/2025", "ALTA");
 
         gimnasio.mostrarNombreGimnasio();
         console.writeln("");
@@ -25,16 +26,27 @@ public class Cliente {
         console.writeln("  " + socio1.preguntarEstadoInscripcion());
         console.writeln("");
 
+        console.write("  \"" + socio2.preguntarNombreSocios() + "\" - DNI: " + socio2.preguntarDNI());
+        console.writeln("  " + socio2.preguntarEstadoInscripcion());
+        console.writeln("");
+
         console.writeln("Reservas:");
         Reservas res1 = new Reservas(socio1.preguntarNombreSocios(), "Yoga", "01/04/2025");
-
         yoga.agregarActividad();
-
-        console.write("  Socio: " + socio1.preguntarNombreSocios() + " en ");
+        console.write("  ->Socio: " + socio1.preguntarNombreSocios());
         yoga.mostrar();
+        yoga.mostrarAforo();
+        console.writeln("Fecha de la reserva: " + socio1.preguntarFechaInscripcion());
 
+        console.writeln("");
+        console.writeln("----------------------------------------------------");
+        console.writeln("");
+
+        Reservas res2 = new Reservas(socio2.preguntarNombreSocios(), "Yoga", "01/04/2025");
+
+        console.write(" ->Socio " + socio2.preguntarNombreSocios() + " ");
+        yoga.agregarActividad();
         yoga.mostrarAforo();
 
-        console.writeln("  Fecha: " + socio1.preguntarFechaInscripcion());
     }
 }
